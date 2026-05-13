d16a1c6408bd7dc14e2f0e7f1d6ca1e99e1c4c2b
阿沈 <149996372+ShilianShen@users.noreply.github.com>
2026-05-13 20:57:59 +0200

    Refactor love2d_draw.lua to optimize node management and improve drawing logic

--- extensions/love2d_draw.lua
+++ extensions/love2d_draw.lua
@@ -5,8 +5,12 @@ local cleanColor = { 0.2, 0.5, 0.3 }
 local shineColor = { 0.9, 0.8, 0.2 }
 local nightColor = { 0.1, 0.1, 0.12 }
 local lightColor = { 0.95, 0.95, 0.9 }
+local smooth = 0.5
 local scaleMax = 1
 local scaleMin = 0.5
+local infoDict = {}
+local mouseX, mouseY
+local winW, winH
 
 local function lpairs(t)
 	local keys = {}
@@ -38,51 +42,51 @@ local function dump(tab, depth, result)
 	return result
 end
 
-local function actv(t)
-	return math.tanh(t / 2)
-end
-
 local function calc(tree)
+	-- remove
+	for tag, _ in pairs(infoDict) do
+		if tree.nodeDict[tag] == nil then
+			infoDict[tag] = nil
+		end
+	end
+
+	-- insert
+	for tag, node in pairs(tree.nodeDict) do
+		if infoDict[tag] == nil then
+			infoDict[tag] = {
+				node = node,
+				text = love.graphics.newText(font1, tag),
+			}
+		end
+	end
+
+	-- mesh
 	local mesh = {}
 	for tag, node in pairs(tree.nodeDict) do
 		mesh[node.depth] = mesh[node.depth] or {}
 		table.insert(mesh[node.depth], tag)
 	end
+	for _, row in ipairs(mesh) do
+		table.sort(row)
+	end
 
-	local visual = {}
-	local winW, winH = love.graphics.getDimensions()
-	local mouseX, mouseY = love.mouse.getPosition()
-
+	-- calc
 	local nodeH = winH / (tree.depth + 1)
 	local k = 1
 	for i = 1, #mesh do
 		local nodeW = winW / (#mesh[i] + 1) * k
 		for j = 1, #mesh[i] do
 			local tag = mesh[i][j]
-			local info = {
-				node = tree.nodeDict[tag],
-				x = j * nodeW + winW * (1 - k),
-				y = i * nodeH,
-				text = love.graphics.newText(font1, tag),
-			}
+			local info = infoDict[tag]
+			info.x = j * nodeW + winW * (1 - k)
+			info.y = i * nodeH
 			info.d = ((mouseX - info.x) / nodeW) ^ 2 + ((mouseY - info.y) / nodeH) ^ 2
-			info.s = scaleMax + (scaleMin - scaleMax) * actv(info.d)
+			info.s = scaleMax + (scaleMin - scaleMax) * math.tanh(info.d / 2)
 			info.w = info.s * info.text:getWidth()
 			info.h = info.s * info.text:getHeight()
 			info.r = info.s * font1:getHeight() / 2
-			if not tree.nodeDict[tag].dirty then
-				info.c = cleanColor
-			else
-				info.c = dirtyColor
-			end
-			visual[tag] = info
 		end
 	end
-	return visual
-end
-
-local function comp(info, jnfo)
-	return info.s < jnfo.s
 end
 
 local function drawEntity(tab, depth, indent)
@@ -90,42 +94,22 @@ local function drawEntity(tab, depth, indent)
 	indent = indent or 16
 	local strings = dump(tab, depth)
 	local x, y = 0, (love.graphics.getHeight() - font2:getHeight() * #strings) / 2
+	love.graphics.setFont(font2)
 	for _, string in ipairs(strings) do
-		if string == "}" then
-			x = x - indent
-		end
-
-		local text = love.graphics.newText(font2, string)
+		x = x - (string == "}" and indent or 0)
 		love.graphics.setColor(nightColor)
-		love.graphics.rectangle("fill", x, y, text:getWidth(), text:getHeight())
-
+		love.graphics.rectangle("fill", x, y, font2:getWidth(string), font2:getHeight())
 		love.graphics.setColor(lightColor)
-		love.graphics.draw(text, x, y)
-
-		y = y + text:getHeight()
-
-		if string.match(string, "{") then
-			x = x + indent
-		end
+		love.graphics.print(string, x, y)
+		y = y + font2:getHeight()
+		x = x + (string.match(string, "{") and indent or 0)
 	end
 end
 
 local function drawEdge(info1, info2, theme)
 	love.graphics.setColor(theme or lightColor)
-	local smooth = 0.5
-	local vertices = {
-		info1.x,
-		info1.y,
-
-		info1.x,
-		(1 - smooth) * info1.y + smooth * info2.y,
-
-		info2.x,
-		smooth * info1.y + (1 - smooth) * info2.y,
-
-		info2.x,
-		info2.y,
-	}
+	local dy = smooth * (info2.y - info1.y)
+	local vertices = { info1.x, info1.y, info1.x, info1.y + dy, info2.x, info2.y - dy, info2.x, info2.y }
 	local curve = love.math.newBezierCurve(vertices)
 	love.graphics.line(curve:render())
 end
@@ -137,42 +121,35 @@ local function drawNode(info, theme)
 	love.graphics.setColor(theme or lightColor)
 	love.graphics.circle("line", info.x, info.y, info.r)
 
-	love.graphics.setColor(info.c)
+	love.graphics.setColor(info.node.dirty and dirtyColor or cleanColor)
 	love.graphics.rectangle("fill", info.x, info.y, info.w, info.h)
 
 	love.graphics.setColor(theme or lightColor)
 	love.graphics.draw(info.text, info.x, info.y, 0, info.s, info.s)
 end
 
-local function draw(tree, visual)
-	local mouseX, mouseY = love.mouse.getPosition()
-	local indices = {}
-	for _, info in pairs(visual) do
-		indices[#indices + 1] = info
-	end
-	table.sort(indices, comp)
-
+local function draw(tree)
 	for tag, node in pairs(tree.nodeDict) do
-		local info1 = visual[tag]
+		local info1 = infoDict[tag]
 		for tag2, _ in pairs(node.children) do
-			local info2 = visual[tag2]
+			local info2 = infoDict[tag2]
 			drawEdge(info1, info2)
 		end
 	end
 
-	for _, info in ipairs(indices) do
+	for _, info in pairs(infoDict) do
 		drawNode(info)
 	end
 
-	for _, info in pairs(visual) do
+	for _, info in pairs(infoDict) do
 		local dist = (mouseX - info.x) ^ 2 + (mouseY - info.y) ^ 2
 		if dist < info.r ^ 2 then
 			drawEntity(info.node.entity)
 			for tag2, _ in pairs(info.node.children) do
-				drawEdge(info, visual[tag2], shineColor)
+				drawEdge(info, infoDict[tag2], shineColor)
 			end
 			for tag2, _ in pairs(info.node.parents) do
-				drawEdge(info, visual[tag2], shineColor)
+				drawEdge(info, infoDict[tag2], shineColor)
 			end
 			drawNode(info, shineColor)
 			break
@@ -180,12 +157,14 @@ local function draw(tree, visual)
 	end
 end
 
-local function soft(tree)
+local function call(tree)
+	winW, winH = love.graphics.getDimensions()
+	mouseX, mouseY = love.mouse.getPosition()
 	love.graphics.push("all")
 	love.graphics.setLineWidth(1)
-	local visual = calc(tree)
-	draw(tree, visual)
+	calc(tree)
+	draw(tree)
 	love.graphics.pop()
 end
 
-return soft
+return call
